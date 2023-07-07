c365bb28743ae018acacef74794998e6816e7adb
Parvin-Gasimli <parvinzga@code.edu.az>
2023-07-07 12:14:01 +0400

    add Saga

--- ReduxSaga/src/store/action.js
+++ ReduxSaga/src/store/action.js
@@ -4,8 +4,10 @@ import {
   USER_FETCH_SUCCEEDED,
 } from "./actionTypes";
 
+//Call Helper**
 import CreateAction from "../helper/action-helper";
 
+//Action Creator**
 export const requestAction = CreateAction(USER_FETCH_REQUESTED);
 export const successAction = CreateAction(USER_FETCH_SUCCEEDED);
 export const failureAction = CreateAction(USER_FETCH_FAILED);
